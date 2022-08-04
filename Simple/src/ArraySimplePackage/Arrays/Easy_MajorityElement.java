0e7c90189a3b9b263cbb1422758aa59a48b44837
Akhilesh <akhilesh.chaurasiya@pb.com>
2022-08-04 13:51:27 +0530

    Added from Array Set 3

---
+++ Simple/src/ArraySimplePackage/Arrays/Easy_MajorityElement.java
@@ -0,0 +1,41 @@
+/*
+Problem Description
+Given an array of size N, find the majority element. The majority element is the element that appears more than floor(n/2) times.
+You may assume that the array is non-empty and the majority element always exists in the array.
+
+
+
+Problem Constraints
+1 <= N <= 5*105
+1 <= num[i] <= 109
+
+Solution approach |==>
+Freq=1 curItem=A[0]
+increse the freq if same element found
+decrease the frequency if same as previous element not found
+if (freq==0) set curItem=A[i] and freq=1
+
+ */
+package ArraySimplePackage.Arrays;
+
+public class Easy_MajorityElement {
+    public int majorityElement(final int[] A) {
+        int currentItem=A[0];
+        int frequency=1;
+        for(int i=1;i<A.length;i++){
+            if(A[i]!=currentItem){
+                frequency--;
+                if(frequency==0)
+                {
+                    currentItem=A[i];
+                    frequency=1;
+                }
+
+            }
+            else
+                frequency++;
+
+        }
+        return currentItem;
+    }
+}
---
+++ Simple/src/ArraySimplePackage/Arrays/Medium_MajorityElement_N3.java
@@ -0,0 +1,69 @@
+
+/*
+(Q)## N/3 Repeat Number ##
+Problem Description
+You're given a read-only array of N integers. Find out if any integer occurs more than N/3 times in the array in linear time and constant additional space.
+If so, return the integer. If not, return -1.
+
+If there are multiple solutions, return any one.
+
+
+
+Problem Constraints
+1 <= N <= 7*105
+1 <= A[i] <= 109
+solution approach
+item1,item2 and freq1,freq2
+
+get item1 and item2 check if anyone of it have really freqency >N/3
+
+ */
+package ArraySimplePackage.Arrays;
+
+import java.util.List;
+
+public class Medium_MajorityElement_N3 {
+    public int repeatedNumber(final List<Integer> a) {
+        //return 0;
+        int currentItem=-1;
+        int frequency=0;
+        int currentItem2=-1;
+        int frequency2=0;
+        for(int i=0;i<a.size();i++){
+            if(currentItem==a.get(i)){
+                frequency++;
+            }
+            else if(currentItem2==a.get(i)){
+                frequency2++;
+            }
+            else if(frequency==0){
+                currentItem=a.get(i);
+                frequency=1;
+            }
+            else if(frequency2==0){
+                currentItem2=a.get(i);
+                frequency2=1;
+            }
+            else
+            {
+                frequency--;
+                frequency2--;
+            }
+
+
+        }
+        int f1=0,f2=0;
+        for(int i=0;i<a.size();i++){
+            if(a.get(i)==currentItem)
+                f1++;
+            else if(a.get(i)==currentItem2)
+                f2++;
+        }
+        if(f1>a.size()/3)
+            return currentItem;
+        else if(f2>a.size()/3)
+            return currentItem2;
+        else
+            return -1;
+    }
+}
