68b60ddb42d5e4e35bdce4d4a2382571722cc54d
Akhilesh chaurasiya <akhilesh.chaurasiya@gmail.com>
2023-11-23 00:47:41 +0530

    Update MergeOvelappingSubInterwal.java

--- Simple/src/Arrays/All/MergeOvelappingSubInterwal.java
+++ Simple/src/Arrays/All/MergeOvelappingSubInterwal.java
@@ -30,3 +30,31 @@ public class MergeOvelappingSubInterwal {
         return ans;
     }
 }
+//Better solution aray only
+class Solution {
+    public int[][] merge(int[][] intervals) {
+         //sort the input on start index
+        int N=intervals.length;
+        List<int[]> resultList=new ArrayList<>();
+        Arrays.sort(intervals,(i1,i2)->i1[0]-i2[0]);
+        int lastStart=intervals[0][0];
+        int lastEnd=intervals[0][1];
+        for(int i=1;i<N;i++)
+        {
+            if(intervals[i][0]<=lastEnd)//definately an overlap start, just need to check on overlap end
+            {
+                lastEnd=Math.max(lastEnd,intervals[i][1]);
+            }
+            else
+            {
+                resultList.add(new int[]{lastStart,lastEnd});//add last and treat next as the first one
+                lastStart=intervals[i][0];
+                lastEnd=intervals[i][1];
+            }
+        }
+
+        resultList.add(new int[]{lastStart,lastEnd});
+        return resultList.toArray(new int[resultList.size()][]); 
+
+    }
+}
