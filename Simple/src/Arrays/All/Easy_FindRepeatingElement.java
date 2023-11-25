801ffe4de80495ac145f4e46c1bd56a4a47ed3c2
Akhilesh chaurasiya <akhilesh.chaurasiya@gmail.com>
2023-11-25 14:32:23 +0530

    Update Easy_FindRepeatingElement.java

--- Simple/src/Arrays/All/Easy_FindRepeatingElement.java
+++ Simple/src/Arrays/All/Easy_FindRepeatingElement.java
@@ -6,7 +6,27 @@ We need to find the element that occurs more than once and whose index of the fi
 
 If there is no repeating element, return -1.
 
+Example Input
 
+Input 1:
+
+ A = [10, 5, 3, 4, 3, 5, 6]
+
+Input 2:
+
+ A = [6, 10, 5, 4, 9, 120]
+
+
+
+Example Output
+
+Output 1:
+
+ 5
+
+Output 2:
+
+ -1
 
 Problem Constraints
 1 <= N <= 105
