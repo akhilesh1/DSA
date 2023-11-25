74cb7f8bb0fda324da17669384b55747d46faa88
Akhilesh chaurasiya <akhilesh.chaurasiya@gmail.com>
2023-11-25 16:56:01 +0530

    Update Easy_MultipleLeftRotations.java

--- Simple/src/Arrays/All/Easy_MultipleLeftRotations.java
+++ Simple/src/Arrays/All/Easy_MultipleLeftRotations.java
@@ -4,6 +4,18 @@ Given an array of integers A and multiple values in B, which represents the numb
 
 Find the rotated array for each value and return the result in the from of a matrix where ith row represents the rotated array for the ith value in B.
 
+Input 1:
+    A = [1, 2, 3, 4, 5]
+    B = [2, 3]
+Input 2:
+    A = [5, 17, 100, 11]
+    B = [1]
+    
+Output 1: 
+    [ [3, 4, 5, 1, 2]
+     [4, 5, 1, 2, 3] ]
+Output 2:
+    [ [17, 100, 11, 5] ]
 
 
 Problem Constraints
