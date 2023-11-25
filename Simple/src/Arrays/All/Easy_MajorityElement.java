34be26c0c735647e8ea72af2d3fa798204fa1ce2
Akhilesh chaurasiya <akhilesh.chaurasiya@gmail.com>
2023-11-25 16:16:34 +0530

    Update Easy_MajorityElement.java

--- Simple/src/Arrays/All/Easy_MajorityElement.java
+++ Simple/src/Arrays/All/Easy_MajorityElement.java
@@ -3,6 +3,18 @@ Problem Description
 Given an array of size N, find the majority element. The majority element is the element that appears more than floor(n/2) times.
 You may assume that the array is non-empty and the majority element always exists in the array.
 
+Example Input
+Input 1:
+[2, 1, 2]
+Input 2:
+[1, 1, 1]
+
+
+Example Output
+Input 1:
+2
+Input 2:
+1
 
 
 Problem Constraints
