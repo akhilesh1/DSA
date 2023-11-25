1ab7cb1fe029142b10cfc8763053485ddb4f71b0
Akhilesh chaurasiya <akhilesh.chaurasiya@gmail.com>
2023-11-25 13:34:42 +0530

    Update Easy_AG_SuSequence.java

--- Simple/src/Arrays/All/Easy_AG_SuSequence.java
+++ Simple/src/Arrays/All/Easy_AG_SuSequence.java
@@ -6,7 +6,27 @@ You have to find how many times subsequence "AG" is there in the given string.
 
 NOTE: Return the answer modulo 109 + 7 as the answer can be very large.
 
+Example Input
 
+Input 1:
+
+ A = "ABCGAG"
+
+Input 2:
+
+ A = "GAB"
+
+
+
+Example Output
+
+Output 1:
+
+ 3
+
+Output 2:
+
+ 0
 
 Problem Constraints
 1 <= length(A) <= 105
