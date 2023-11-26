c59a841aec47c0dac13985a25ee945de8da9b255
Akhilesh chaurasiya <akhilesh.chaurasiya@gmail.com>
2023-11-26 16:33:41 +0530

    Update Easy_StockBuySell_MaxProfit.java

--- Simple/src/Arrays/All/Easy_StockBuySell_MaxProfit.java
+++ Simple/src/Arrays/All/Easy_StockBuySell_MaxProfit.java
@@ -1,6 +1,7 @@
 /*
 You are given an array/list 'prices' where the elements of the array represent the prices of the stock
-as they were yesterday and indices of the array represent minutes. Your task is to find and return the maximum profit you can make by buying and selling the stock.
+as they were yesterday and indices of the array represent minutes. 
+Your task is to find and return the maximum profit you can make by buying and selling the stock.
 You can buy and sell the stock only once.
 
 
