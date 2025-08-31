b5bbdd4d6541347f390d2d3e06056c760e2fccc5
Akhilesh chaurasiya <akhilesh.chaurasiya@gmail.com>
2025-08-31 20:25:21 +0530

    Time: 3 ms (49.61%) | Memory: 42.8 MB (9.64%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public bool IsValid(string s) {
+        int N=s.Length;
+        //["()[]{}"
+        //["([])"
+        var pairs="()[]{}";
+        Stack<char> st=new();
+        for(int i=0;i<N;i++)
+        {
+            if(pairs.IndexOf(s[i])%2==0)
+            {
+                st.Push(s[i]);
+            }
+            else
+            {
+                if (st.Count==0 || pairs.IndexOf(st.Peek())!=pairs.IndexOf(s[i])-1)
+                    return false;
+                else
+                    st.Pop();
+            }
+        }
+        return st.Count==0;
+    }
+}
