68bdff6df7a48b6853f5a8c45e746769c08596b7
Akhilesh chaurasiya <akhilesh.chaurasiya@gmail.com>
2025-09-07 11:32:41 +0530

    Time: 7 ms (8.86%) | Memory: 50.1 MB (68.73%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cs
+++ 22-generate-parentheses/generate-parentheses.cs
@@ -1,27 +1,30 @@
 public class Solution {
 
-    void recursive(string cur,int opened,int closed,List<string> ans,int n)
-    {
-        if(opened==closed && opened==n)
-        {
-            ans.Add(cur);
-            return;
-        }
-
-        if(opened<n)//3,2
-        {
-            recursive(cur+"(",opened+1,closed,ans,n);
-        }
-        if(closed<opened)//3,2
-        {
-            recursive(cur+")",opened,closed+1,ans,n);
-        }
-        
+    public IList<string> GenerateParenthesis(int n) {
+        List<string> ans=new();
+        Stack<char> stc=new();
+        backtrack(n,0,0,ans,stc);
+        return ans;
     }
 
-    public IList<string> GenerateParenthesis(int n) {
-        var ans=new List<string>();
-        recursive("",0,0,ans,n);
-        return ans;
+    public void backtrack(int n,int opened,int closed,List<string> ans,Stack<char> stc)
+    {
+        if(opened==n && opened==closed)
+        {
+            ans.Add(new string(stc.Reverse().ToArray()));
+        }
+
+        if(opened<n)
+        {
+            stc.Push('(');
+            backtrack(n,opened+1,closed,ans,stc);
+            stc.Pop();
+        }
+        if(closed<opened)
+        {
+            stc.Push(')');
+            backtrack(n,opened,closed+1,ans,stc);
+            stc.Pop();
+        }
     }
 }
