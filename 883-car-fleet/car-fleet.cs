6f71442c914e16c5da4213fd61abc112ef0a0992
Akhilesh chaurasiya <akhilesh.chaurasiya@gmail.com>
2025-09-09 23:18:35 +0530

    Time: 155 ms (21.16%) | Memory: 74 MB (30.03%) - LeetSync

--- 883-car-fleet/car-fleet.cs
+++ 883-car-fleet/car-fleet.cs
@@ -1,16 +1,20 @@
 public class Solution {
     public int CarFleet(int target, int[] position, int[] speed) {
         int n = position.Length;
-        var cars = position.Zip(speed, (p, s) => (p, s)).OrderBy(c => c.p).ToList();
-        var stack = new Stack<double>();
+        var cars = new List<(int p, int s)>();
+        for (int i = 0; i < n; i++) cars.Add((position[i], speed[i]));
+        cars.Sort((a, b) => a.p.CompareTo(b.p));
+
+        int fleets = 0;
+        double lastTime = -1;
         for (int i = n - 1; i >= 0; i--) {
             double time = (double)(target - cars[i].p) / cars[i].s;
-            //stack is relevant here just to chech one car ahead.
-            // time is current car time taken to reach target
-            //if current time taken is greter then just ahead car it will form a new fleet
-            if(stack.Count==0 || stack.Peek()<time)// if 
-                stack.Push(time);
+            if (time > lastTime) {
+                fleets++;
+                lastTime = time;
+            }
+            // else this car catches up to the fleet ahead
         }
-        return stack.Count; //total fleets
+        return fleets;
     }
 }
