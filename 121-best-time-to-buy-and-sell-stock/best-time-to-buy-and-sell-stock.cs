30ec3c6fd4f48d2525af7edba2576ae2ac787846
Akhilesh chaurasiya <akhilesh.chaurasiya@gmail.com>
2025-09-15 00:40:31 +0530

    Time: 2 ms (43.83%) | Memory: 58.7 MB (76.89%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int profit=0;
+        int min=prices[0];
+        int N=prices.Length;
+        for(int i=1;i<N;i++)
+        {
+            min=int.Min(min,prices[i]);//7,1,1,1
+            profit=int.Max(profit,prices[i]-min);//0,0,4,4,6
+        }
+        return profit;
+        //
+    }
+}
