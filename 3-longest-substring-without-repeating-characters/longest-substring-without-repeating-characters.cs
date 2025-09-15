bc1e88c0a5c862171914ab862fe2329a0bec71db
Akhilesh chaurasiya <akhilesh.chaurasiya@gmail.com>
2025-09-15 22:37:49 +0530

    Time: 6 ms (57.36%) | Memory: 44 MB (59.13%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
@@ -1,21 +1,29 @@
 public class Solution {
     public int LengthOfLongestSubstring(string s) {
-    int N = s.Length;
-    int left = 0, right = 0;
-    int ans = 0;
-    HashSet<char> set = new();
+        //how sliding would help in creating non repeating substring
+        int N=s.Length;
+        int ans=1;
+        int left=0;
+        int right=0;
+        if (N == 0) return 0;
+        HashSet<char> set=new();
+        //set.Add(s[0]);
+        while(right<=N-1)
+        {
+            if(set.Contains(s[right]))
+            {// not sure what to move
+                set.Remove(s[left]);
+                left++;
+            }
+            else
+            {
+                set.Add(s[right]);
+                ans=Math.Max(ans,set.Count);
+                right++;
+                
+            }
 
-    while (right < N) {
-        if (set.Contains(s[right])) {
-            set.Remove(s[left]);
-            left++;
-        } 
-        else {
-            set.Add(s[right]);
-            ans = Math.Max(ans, set.Count);
-            right++;
         }
+        return ans;
     }
-    return ans;
-}
 }
