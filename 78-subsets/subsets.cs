54efd0b589508f8a16ebeea40353b64668077581
Akhilesh chaurasiya <akhilesh.chaurasiya@gmail.com>
2025-11-17 00:46:31 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (35.50%) - LeetSync

--- 78-subsets/subsets.cs
+++ 78-subsets/subsets.cs
@@ -1,33 +1,20 @@
 public class Solution {
     public IList<IList<int>> Subsets(int[] nums) {
-        IList<IList<int>> ans=new List<IList<int>>();
-        //decalre ANS
-        DFS(ans,new List<int>(),0,nums);
-        // CALL DFS
+        List<IList<int>> ans=new();
+        Backtrack(nums,0,new List<int>(),ans);
         return ans;
     }
 
-    public void DFS(IList<IList<int>> ans,List<int> current,int idx,int[] nums)
+    public void Backtrack(int[] nums,int idx,List<int> cur,List<IList<int>> ans)
     {
-
-        
-        
-        //base condition return
         if(idx==nums.Length)
         {
-            ans.Add(new List<int>(current));
+            ans.Add(new List<int>(cur));
             return;
         }
-
-        
-
-        DFS(ans,new List<int>(current),idx+1,nums);
-
-        var includedList=new List<int>(current);
-        includedList.Add(nums[idx]);
-
-        DFS(ans,includedList,idx+1,nums);
-
+        cur.Add(nums[idx]);
+        Backtrack(nums,idx+1,cur,ans);
+        cur.RemoveAt(cur.Count-1);
+        Backtrack(nums,idx+1,cur,ans);
     }
-
 }
