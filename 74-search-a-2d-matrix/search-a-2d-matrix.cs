062c268a945f0795ce124f6e15af74d51e78033f
Akhilesh chaurasiya <akhilesh.chaurasiya@gmail.com>
2025-12-10 23:56:18 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (5.41%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cs
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cs
@@ -1,37 +1,24 @@
 public class Solution {
     public bool SearchMatrix(int[][] matrix, int target) {
-        int rows=matrix.Length;
-        int cols=matrix[0].Length;
-        int N=rows*cols;
-
-        int left=0;
-        int right=N-1;
-        while(left<=right)
+        int l=matrix.Length*matrix[0].Length;
+        int start=0;
+        int end=l-1;
+        while(start<=end)
         {
-            int mid=(left+right)/2;
-            int cur=getValue(mid,matrix);
+            int mid=(start+end)/2;
+            int cur=GetNumber(mid,matrix);
             if(cur==target) return true;
-            else if(cur<target)
-            {
-                left=mid+1;
-            }
-            else
-            {
-                right=mid-1;
-            }
+            else if(cur<target) start=mid+1;
+            else end=mid-1;
         }
         return false;
     }
-
-    int getValue(int i,int[][] matrix)
+    private int GetNumber(int idx,int[][] matrix)
     {
-        //r =1 c=2 n 6
-        int rows=matrix.Length;
-        int cols=matrix[0].Length;
-        int r=i/cols;//6/4= 1
-        int c=i%cols;//6%3 =3
-
-        return matrix[r][c];
+        int m=matrix.Length;
+        int n=matrix[0].Length;
+        int i=(idx)/n;
+        int j=(idx)%n;
+        return matrix[i][j];
     }
-
 }
